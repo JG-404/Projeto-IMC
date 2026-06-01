61afbea6f07e9b0b83aff936138182614739fb23
JG-404 <jotagemurerleite@gmail.com>
2026-06-01 15:44:22 -0300

    api criada

--- API/index.js
+++ API/index.js
@@ -3,9 +3,9 @@ const app = require('./src/app')
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-    console.log('============================')
-    console.log(`Api rodando na porta: ${PORT}`)
-    console.log('Para ver o status acesse:')
-    console.log(`http://localhost:8080/health`)
-    console.log('============================')
+    console.log('================================')
+    console.log(`API rodando na porta: ${PORT}`)
+    console.log(`Para consultar os status acesse:`)
+    console.log(`http:localhost:${PORT}/health`)
+    console.log('================================')
 })
--- API/src/app.js
+++ API/src/app.js
@@ -1,32 +1,33 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
-app.get('/health', (req, res) => { 
+app.get('/health', (req,res) =>{
     res.json({
         status: 'ok',
         timestamp: new Date().toISOString(),
-        message: 'Hello World! :)'
+        message: "Hello World! :D"
     })
 })
 
-app.post('/imc/', (req, res) => {
-    try{    
+app.post('/imc', (req,res) => {
+    try{
         const data = req.body
-        
         const {peso = 0, altura = 0} = data
 
         if (peso <= 0) throw new Error("Peso invalido")
         if (altura <= 0) throw new Error("Altura invalida")
 
-        const imc = peso * (altura ** 2)
+        const imc = peso / (altura ** 2)
 
         res.status(200).json({resultado: imc})
     }
     catch(err){
-        res.status(406).json({resultado: "erro"})
+        res.status(406).json({erro: err})
     }
 })
 
